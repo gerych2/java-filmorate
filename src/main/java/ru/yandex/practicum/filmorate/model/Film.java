c54d801b844f6068463c610bff646e8f93e07c69
German-Trosko <skodagrodno2006@gmail.com>
2025-08-04 11:24:20 +0300

    попытка 18

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,31 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Film {
     private int id;
 
-    @NotBlank(message = "Название фильма не может быть пустым")
+    @NotBlank(message = "Название не может быть пустым")
     private String name;
 
-    @Size(max = 200, message = "Описание не должно превышать 200 символов")
+    @Size(max = 200, message = "Описание не может превышать 200 символов")
     private String description;
 
-    @NotNull(message = "Дата релиза обязательна")
+    @NotNull(message = "Дата релиза не может быть пустой")
     private LocalDate releaseDate;
 
     @Positive(message = "Продолжительность должна быть положительной")
     private int duration;
 
-    @Builder.Default
     private Set<Integer> likes = new HashSet<>();
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,36 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import lombok.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     private int id;
 
+    @Email(message = "Email должен быть валидным")
     @NotBlank(message = "Email не может быть пустым")
-    @Email(message = "Некорректный email")
     private String email;
 
     @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелы")
     private String login;
 
     private String name;
 
-    @NotNull(message = "Дата рождения не может быть пустой")
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    @NotNull(message = "Дата рождения обязательна")
+    @Past(message = "Дата рождения должна быть в прошлом")
     private LocalDate birthday;
 
-    @Builder.Default
     private Set<Integer> friends = new HashSet<>();
 
     public void fillNameIfEmpty() {
