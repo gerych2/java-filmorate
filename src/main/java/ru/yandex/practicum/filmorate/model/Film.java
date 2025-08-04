bc596674354e56c4ea099506c3253bec42188ded
German-Trosko <skodagrodno2006@gmail.com>
2025-08-04 11:59:21 +0300

    попытка 27

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -20,6 +21,7 @@ public class Film {
     @Size(max = 200, message = "Описание не может превышать 200 символов")
     private String description;
 
+    @JsonProperty(required = true)
     @NotNull(message = "Дата релиза не может быть пустой")
     private LocalDate releaseDate;
 
